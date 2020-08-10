f744acfc47e59db462d51de2cf8c78f94b00c705
Wenyan Ma <mwenyan@tibco.com@Wenyans-MacBook-Pro.local>
2020-08-10 16:53:45 -0400

    cleanup testing

--- scripts/step-1.1-unit-test.sh
+++ scripts/step-1.1-unit-test.sh
@@ -1,2 +1,2 @@
-./engine-darwin_amd64 --app ../flight_blueeye_ingestion_txf/flogo.json -test -flows
-./engine-darwin_amd64 --app ../flight_blueeye_ingestion_txf/flogo.json -test -flowin toJson_unit_test.json -flowout output.json
+./engine-darwin_amd64 -app ../flight_blueeye_ingestion_txf/flogo.json -test -flows
+./engine-darwin_amd64 -app ../flight_blueeye_ingestion_txf/flogo.json -test -flowin toJson_unit_test.json -flowout output.json
